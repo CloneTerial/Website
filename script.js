3d8a8838531b4ae645f9fa91f49d4c64c679d3ce
CloneTerial <yugadanjanu123@gmail.com>
2024-08-25 19:15:13 +0700

    Styling

---
+++ script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    window.onload = function() {
+        var loadingScreen = document.getElementById("loading-screen");
+        var content = document.getElementById("content");
+
+        // Hide the loading screen
+        loadingScreen.style.display = "none";
+        // Show the main content
+        content.style.display = "block";
+    };
+});
+
+   
+        
+
+  // Tambahkan event listener untuk klik
+ 
