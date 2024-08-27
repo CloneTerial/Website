056ac4fee3d8454cd78ab9a19de484365c1b50ae
CloneTerial <yugadanjanu123@gmail.com>
2024-08-27 20:33:46 +0700

    styling

--- script.js
+++ script.js
@@ -7,12 +7,29 @@ document.addEventListener("DOMContentLoaded", function() {
         // Hide the loading screen
         loadingScreen.style.display = "none";
         // Show the main content
-        content.style.display = "block";
+         
     };
 });
 
-   
-        
+ // Inisialisasi variabel audio dan status pemutaran
+let currentAudio = new Audio('./est ce que tu maimes.mp3'); // Path ke file audio lokal
+let isPlaying = false;
+function playPreview() {
+    if (isPlaying) {
+        currentAudio.pause();
+        document.getElementById('uu').classList.remove('playing'); // Menghapus outline saat dipause
+    } else {
+        currentAudio.play();
+        document.getElementById('uu').classList.add('playing'); // Menambahkan outline saat diputar
+    }
+    isPlaying = !isPlaying; // Toggle status pemutaran
+}
 
-  // Tambahkan event listener untuk klik
- 
+// Menambahkan event listener ke elemen gambar
+document.getElementById('uu').addEventListener('click', playPreview);
+
+// Mengatur status pemutaran saat audio selesai
+currentAudio.onended = () => {
+    isPlaying = false;
+    document.getElementById('uu').classList.remove('playing'); // Menghapus outline saat audio selesai
+};
